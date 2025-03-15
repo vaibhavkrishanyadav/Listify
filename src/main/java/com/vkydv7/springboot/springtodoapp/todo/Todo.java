cf95c483c3b7be84e7c7492876e6ae78dcadcc25
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2025-03-15 23:05:51 +0530

    Add JPA integration and configure H2 database

--- src/main/java/com/vkydv7/springboot/springtodoapp/todo/Todo.java
+++ src/main/java/com/vkydv7/springboot/springtodoapp/todo/Todo.java
@@ -2,10 +2,18 @@ package com.vkydv7.springboot.springtodoapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 
+	public Todo() {
+		
+	}
+	
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
 		this.id = id;
@@ -15,6 +23,8 @@ public class Todo {
 		this.done = done;
 	}
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'vky20', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'vky20', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'vky20', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'vky20', 'Learn DevOps', CURRENT_DATE(), false);
